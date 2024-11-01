4a9c9c7959dd71058c117b41bf5164fcdbe479e3
Keysi <kfajardop@miumg.edu.gt>
2024-11-01 13:51:14 -0600

    Implementacion de proyecto examen final|

--- app/components/layout/full/vertical-sidebar/sidebarItem.ts
+++ app/components/layout/full/vertical-sidebar/sidebarItem.ts
@@ -17,11 +17,37 @@ export interface menu {
 
 const sidebarItem: menu[] = [
     {
-        title: 'Home',
+        title: 'Tareas',
         icon: 'mdi-home',
         value: 'home',
-        to: '/',
+        to: '/tareas',
     },
+    {
+        title: 'Admin',
+        icon: 'mdi-home',
+        value: 'home',
+        children: [
+            {
+                title: 'Tarea Tipos',
+                icon: 'mdi-view-dashboard',
+                value: 'TareaTipo',
+                to: '/tareatipos',
+            },
+            {
+                title: 'Tarea Estados',
+                value: 'TareaEstado',
+                icon: 'mdi-account',
+                to: '/tareaestados',
+            },
+            {
+                title: 'Tarea Prioridades',
+                value: 'TareaPrioridad',
+                icon: 'mdi-account-group',
+                to: '/tareaprioridades',
+            },
+        ],
+    },
+
 ];
 
 
